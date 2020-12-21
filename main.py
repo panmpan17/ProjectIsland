272a67a424fe2ca9b47bf6d0e453fde39a8b4f92
Michael Pan <panmpan@gmail.com>
2020-12-21 22:16:07 +0800

    Try wsgi fit in pythonanywhere

--- main.py
+++ main.py
@@ -4,3 +4,5 @@ from src import Server
 if __name__ == "__main__":
     server = Server("virtual_root")
     server.run()
+    # application = server.wsgi_application()
+    # Server.test_wsgi_application(application)
--- src/server.py
+++ src/server.py
@@ -1,5 +1,6 @@
 import cherrypy
 import os
+import sys
 
 from .model import Base
 from .view_app import MainView
@@ -16,35 +17,52 @@ class Server:
         }
     }
 
+    @staticmethod
+    def test_wsgi_application(application):
+        cherrypy.tree.graft(application, '/')
+
     def __init__(self, virtual_root_path):
-        sa_plugin = SAPlugin(cherrypy.engine)
-        sa_plugin.to_sqlite("server.db")
-        sa_plugin.Base = Base
-        sa_plugin.subscribe()
+        self.sa_plugin = SAPlugin(cherrypy.engine)
+        self.sa_plugin.to_sqlite("server.db")
+        self.sa_plugin.Base = Base
+        self.sa_plugin.subscribe()
 
-        key_plugin = KeyMgrPlugin(cherrypy.engine, virtual_root_path)
-        key_plugin.subscribe()
+        self.key_plugin = KeyMgrPlugin(cherrypy.engine, virtual_root_path)
+        self.key_plugin.subscribe()
 
-        cherrypy.tools.dbtool = SATool(sa_plugin)
-        cherrypy.tools.keytool = KeyMgrTool(key_plugin)
+        cherrypy.tools.dbtool = SATool(self.sa_plugin)
+        cherrypy.tools.keytool = KeyMgrTool(self.key_plugin)
 
     def run(self):
         cherrypy.quickstart(MainView(), "/", self.CONFIG)
     
+    def stop_wsgi(self):
+        print("Stopping the wsgi...")
+        self.sa_plugin.stop()
+        self.key_plugin.stop()
+        cherrypy.engine.stop()
+    
     def wsgi_application(self):
-        import sys
+        import atexit
+        import signal
 
         sys.stdout = sys.stderr
 
-        cherrypy.config.update({"enviroment": "embedded"})
+        cherrypy.config.update({"enviroment": "production"})
 
-        if cherrypy.__version__.startswith('3.0') and cherrypy.engine.state == 0:
-            import atexit
+        atexit.register(self.stop_wsgi)
+        # signal.signal(signal.SIGKILL, sigHandler)
+        signal.signal(signal.SIGTERM, sigHandler)
+        signal.signal(signal.SIGQUIT, sigHandler)
 
-            cherrypy.engine.start(blocking=False)
-            atexit.register(cherrypy.engine.stop)
+        self.sa_plugin.start()
+        self.key_plugin.start()
         
         application = cherrypy.Application(
-            MainView(), script_name='', config=None)
+            MainView(), script_name='', config=self.CONFIG)
 
         return application
+
+def sigHandler(signo, frame):
+    print("Through signal")
+    sys.exit(0)
